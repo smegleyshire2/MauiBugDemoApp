eded049e2eb7de974b645c5d0dc9bcb36d375289
Adrian Smith <adrian.smith@civica.co.uk>
2024-07-04 18:07:30 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BugDemoApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+
+namespace BugDemoApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<string> Items = new ObservableCollection<string>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindableLayout.SetItemsSource(slItems, Items);
+        }
+
+        private void btnAdd_Clicked(object sender, EventArgs e)
+        {
+            Items.Add($"Item {Items.Count + 1}");
+        }
+
+        private void btnRemove_Clicked(object sender, EventArgs e)
+        {
+            if (Items.Count > 0)
+                Items.RemoveAt(Items.Count - 1);
+        }
+    }
+}
